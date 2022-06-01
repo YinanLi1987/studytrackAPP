6ec4f69a0f80da5d566b56d6c9b2a52a229c11af
YinanLi1987 <t1liyi00@students.oamk.fi>
2022-06-01 11:24:33 +0300

    login test

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.post('/login', async (req, res) => {
   // compare the password
   try {
       if(await bcrypt.compare(req.body.Password, loginUser[0].password) ){
-       redirect('/teacher_home')
+       res.redirect('/teacher_home')
       } else {
           res.send('Incorrect username or password')
         }
