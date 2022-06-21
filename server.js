a09ef5b34e762792cbbedd1c3776a3ca3a8367de
YinanLi1987 <t1liyi00@students.oamk.fi>
2022-06-21 14:49:08 +0300

    login

--- server.js
+++ server.js
@@ -54,8 +54,8 @@ app.get('/db2', async (req, res) => {
     try {
         const client = await pool.connect();
         const result = await client.query('SELECT * FROM courseInfo;');
-        //const results = { 'results': (result) ? result.rows : null};
-        res.json( result );
+        const results =  (result) ? result.rows : null;
+        res.json( results );
         client.release();
     } catch (err) {
           console.error(err);
