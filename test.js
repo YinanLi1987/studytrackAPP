cb7ad862b742c12cb7db32ce03ca72edec755f66
YinanLi1987 <t1liyi00@students.oamk.fi>
2022-06-25 01:26:02 +0300

    show course list

--- test.js
+++ test.js
@@ -5,23 +5,30 @@ let data =[
     "credits":5,
     "numberoflectures":8,
     "examdate":"2022-07-10",
-    "students":
-              "{\"liyinan@gmail.com\",\"liyinan00@gmail.com\",\"liyinan01@gmail.com\"}"},
+    "students":null},
     {"courseid":11,
     "coursename":"002Finnish",
     "coursecontent":"Finnish language and culture",
-    "credits":5,"numberoflectures":8,
+    "credits":5,
+    "numberoflectures":8,
     "examdate":"2022-07-10",
-    "students":"{\"liyinan001@gmail.com\",\"liyinan002@gmail.com\"}"}
+    "students":null},
+    {"courseid":12,
+    "coursename":"003Object Oriented Programming",
+    "coursecontent":"rtyuiopåghjklöävbnm,.-",
+    "credits":5,
+    "numberoflectures":8,
+    "examdate":"2022-06-26",
+    "students":["liyinan001@gmail.com","liyinan002@gmail.com"]}
 ]
 let result=data[0];
 let result2=data[0].students;
-let result3=data[0].students[0];
-let result4=data[0].students[2];
+//let result3=data[0].students[0];
+let result4=data[2].students[1];
 
-let result5=data[0].students[1];
+let result5=data[2].students[1];
 console.log(result);
 console.log(result2);
-console.log(result3);
+//console.log(result3);
 console.log(result4);
 console.log(result5);
