1e236fe062ccdfe345c29bb1dcf494c6bc38a47e
YinanLi1987 <t1liyi00@students.oamk.fi>
2022-06-25 01:43:41 +0300

    show course list

--- test.js
+++ test.js
@@ -1,18 +1,5 @@
 let data =[
-    {"courseid":10,
-    "coursename":"001  Finnish",
-    "coursecontent":"Finnish language and culture",
-    "credits":5,
-    "numberoflectures":8,
-    "examdate":"2022-07-10",
-    "students":null},
-    {"courseid":11,
-    "coursename":"002Finnish",
-    "coursecontent":"Finnish language and culture",
-    "credits":5,
-    "numberoflectures":8,
-    "examdate":"2022-07-10",
-    "students":null},
+    
     {"courseid":12,
     "coursename":"003Object Oriented Programming",
     "coursecontent":"rtyuiopåghjklöävbnm,.-",
@@ -22,10 +9,8 @@ let data =[
     "students":["liyinan001@gmail.com","liyinan002@gmail.com"]}
 ]
 
-function getCourse(){
-for(var i in data){
+
     if(data[i].students.includes("liyinan002@gmail.com")==true){
-       return data[i].cousename;
+       return data[i].coursename;
        }
-}};
-getCourse();
+
