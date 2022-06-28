843d92b49aa82bb2532f68705d38028bb41178bb
YinanLi1987 <t1liyi00@students.oamk.fi>
2022-06-28 21:34:32 +0300

    traffic light

--- server.js
+++ server.js
@@ -192,9 +192,9 @@ app.post('/login', async (req, res) => {
             });
         })();
       try {
-          const {courseName, lesson01, lesson02, lesson03, lesson04, lesson05, lesson06, lesson07, lesson08,students} = req.body;
+          const {courseName, lesson01, lesson02, lesson03, lesson04, lesson05, lesson06, lesson07, lesson08,lesson09,students} = req.body;
           const client = await pool.connect();
-          client.query('INSERT INTO courseInfo VALUES (DEFAULT,$1, $2, $3,$4,$5,$6,$7,$8,$9,$10)',[courseName, students,lesson01, lesson02, lesson03, lesson04, lesson05, lesson06, lesson07, lesson08]);
+          client.query('INSERT INTO courseInfo VALUES (DEFAULT,$1, $2, $3,$4,$5,$6,$7,$8,$9,$10,$11)',[courseName, students,lesson01, lesson02, lesson03, lesson04, lesson05, lesson06, lesson07, lesson08,lesson09]);
         
       //const results = { 'results': (result) ? result.rows : null};
       //res.json( results );
@@ -223,7 +223,7 @@ app.post('/login', async (req, res) => {
           const studentEmail='liyinan002@gmail.com';
           const courseName='005JAVA';
           const client = await pool.connect();
-          client.query('INSERT INTO trafficLight VALUES (DEFAULT,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10) ',[studentEmail,courseName,lesson01, lesson02, lesson03, lesson04, lesson05, lesson06, lesson07, lesson08, lesson09]);
+          client.query('INSERT INTO trafficLight VALUES (DEFAULT,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) ',[studentEmail,courseName,lesson01, lesson02, lesson03, lesson04, lesson05, lesson06, lesson07, lesson08, lesson09]);
         
       //const results = { 'results': (result) ? result.rows : null};
       //res.json( results );
