492fd631cd00cde252e8ca449a08da272fc33619
YinanLi1987 <t1liyi00@students.oamk.fi>
2022-06-29 22:59:15 +0300

    trackpop

---
+++ test.js
@@ -0,0 +1,28 @@
+let data=[
+{ 
+    "courses":"JAVA",
+    "sudent01":"00001",
+    "sudent02":"00002",
+    "sudent03":"00003"
+
+ },
+ { 
+    "courses":"Finnsh",
+    "sudent01":"00001",
+    "sudent02":"00002",
+    "sudent03":"00003"
+
+ },
+ { 
+    "courses":"Chinese",
+    "sudent01":"00001",
+    "sudent02":"00002",
+    "sudent03":"00003"
+
+ }
+]
+for(var i=0; i<data.length; i++) { 
+  console.log(data[i].courses+data[i].sudent01)
+  console.log(data[i].sudent02)
+
+}
