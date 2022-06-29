5196ebb46da229a8e963a4711e5f64863de66b85
YinanLi1987 <t1liyi00@students.oamk.fi>
2022-06-29 09:40:22 +0300

    add teacher to course data

--- server.js
+++ server.js
@@ -215,9 +215,9 @@ app.post('/login', async (req, res) => {
             });  
         })();
       try {
-          const {courseName, lesson01, lesson02, lesson03, lesson04, lesson05, lesson06, lesson07, lesson08,lesson09,students} = req.body;
+          const {courseName, lesson01, lesson02, lesson03, lesson04, lesson05, lesson06, lesson07, lesson08,lesson09,students,teacher} = req.body;
           const client = await pool.connect();
-          client.query('INSERT INTO courseInfo VALUES (DEFAULT,$1, $2, $3,$4,$5,$6,$7,$8,$9,$10,$11)',[courseName, students,lesson01, lesson02, lesson03, lesson04, lesson05, lesson06, lesson07, lesson08,lesson09]);
+          client.query('INSERT INTO courseInfo VALUES (DEFAULT,$1, $2, $3,$4,$5,$6,$7,$8,$9,$10,$11,$12)',[courseName, students,lesson01, lesson02, lesson03, lesson04, lesson05, lesson06, lesson07, lesson08,lesson09,teacher]);
           for(i=0;i<students.length;i++) {
               client.query('INSERT INTO trafficLight VALUES (DEFAULT,$1, $2, $3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20)',[students[i],courseName,'Unmark','Unmark','Unmark','Unmark','Unmark','Unmark','Unmark','Unmark','Unmark',lesson01, lesson02, lesson03, lesson04, lesson05, lesson06, lesson07, lesson08,lesson09])
           }
