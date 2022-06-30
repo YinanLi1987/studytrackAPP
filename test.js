1d1775aa97126dc68fed62bf033b562149803baa
YinanLi1987 <t1liyi00@students.oamk.fi>
2022-06-30 09:28:06 +0300

    lights display

--- test.js
+++ test.js
@@ -1,28 +1,38 @@
 let data=[
-{ 
-    "courses":"JAVA",
-    "sudent01":"00001",
-    "sudent02":"00002",
-    "sudent03":"00003"
-
- },
- { 
-    "courses":"Finnsh",
-    "sudent01":"00001",
-    "sudent02":"00002",
-    "sudent03":"00003"
-
- },
- { 
-    "courses":"Chinese",
-    "sudent01":"00001",
-    "sudent02":"00002",
-    "sudent03":"00003"
-
- }
+   {"lightid":18,"studentemail":"liyinan001@gmail.com","coursename":"Finnish"},
+   {"lightid":19,"studentemail":"zhang002@gmail.com","coursename":"Finnish"},
+   {"lightid":20,"studentemail":"liyinan001@gmail.com","coursename":"005JAVA"},
+   {"lightid":21,"studentemail":"zhang002@gmail.com","coursename":"005JAVA"}
 ]
-for(var i=0; i<data.length; i++) { 
-  console.log(data[i].courses+data[i].sudent01)
-  console.log(data[i].sudent02)
+let userEmail="liyinan41@gmail.com"
 
-}
+let courseArray=['Finnish','005JAVA'];
+
+let output1='';
+let output2='';               
+                 
+for(i in data){
+    if(courseArray[0]==data[i].coursename ){
+      output1+=data[i].studentemail+'\n'
+                                       
+    }   
+                            
+}  
+
+
+for(i in data){
+  if(courseArray[1]==data[i].coursename ){
+    output2+=data[i].studentemail+'\n'
+                                     
+  }   
+                          
+}  
+                          
+ 
+                      
+let x=courseArray[0]+'\n'+output1
+let y=courseArray[1]+'\n'+output2
+                   
+console.log(x);
+console.log(y);
+             
